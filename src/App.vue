1e6bf13bf4eeb9c577edeec17751341c09fc01ff
Adam Bielecki <theadambielecki@gmail.com>
2020-10-11 13:22:18 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos"/>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data (){
+    return {
+      todos: [
+        {
+          id:1,
+          title: "Todo one",
+          complete: false
+        },
+        {
+          id:1,
+          title: "Todo two",
+          complete: true
+        },
+        {
+          id:1,
+          title: "Todo three",
+          complete: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-bind:class="{'is-complete' : todo.complete}">
+      <h3>{{todo.title}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Todo",
+  props: ["todo"]
+};
+</script>
+
+<style scoped>
+
+.is-complete{
+  text-decoration: line-through;
+}
+</style>
